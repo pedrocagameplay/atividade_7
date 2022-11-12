6203e21e4fa56804619a8402391d6c6e87f902e6
pedrocagameplay <70711288+pedrocagameplay@users.noreply.github.com>
2022-11-12 10:44:18 -0300

    Add files via upload

---
+++ atividade JS/index.js
@@ -0,0 +1,6 @@
+let atual;
+atual = 2022
+const nascimento = prompt("Ano do seu nascimento:")
+const nome = prompt("Nome: ")
+const idade = atual - nascimento
+alert("Óla,seu nome é " + nome + " e você tem " + idade + " anos") 
